95aedb0c6afa62e44cf4d96e483e6b46fb417607
vickyboss <vikaspachar63@gmail.com>
2020-08-31 15:43:37 +0530

    App title changed

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("", (req, res) => {
-  res.send("<h1>Learning git basics
-  
-  </h1>");
+  res.send("<h1>Learning git basics</h1>");
 });
 
 module.exports = app;
