72b17b7cadd304ced37a79a2e7bcc49aee57f53f
vickyboss <vikaspachar63@gmail.com>
2020-08-31 10:05:47 +0530

    Initial express server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("", (req, res) => {
+  res.send("<h1>Testing app</h1>");
+});
+
+module.exports = app;
